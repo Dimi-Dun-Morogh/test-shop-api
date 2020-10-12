350195dcb0436ec677dd637c9c6bdc62e111d97c
Dimi-Dun-Morogh <darnelbro@gmail.com>
2020-10-13 02:23:36 +0300

    fix heroku path & port, final commit

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const http = require('http').createServer(app);
 const ROUTES = require('./routes/index');
 const connectDb = require('./db/db-connect');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.use(cors());
 
 app.use(express.json()); // activate middleware to parse JSON
